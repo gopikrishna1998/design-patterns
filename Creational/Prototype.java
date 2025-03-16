dd7f22e0aac1bd0e4eee2065a977829b4a47fe4d
gopikrishna1998 <32165792+gopikrishna1998@users.noreply.github.com>
2025-03-16 22:29:20 +0530

    Adding Creational/Prototype

---
+++ Creational/Prototype.java
@@ -0,0 +1,55 @@
+// Prototype Interface
+interface Shape extends Cloneable {
+    Shape clone();
+    void draw();
+}
+
+// Concrete Prototype
+class Circle implements Shape {
+    private int x;
+    private int y;
+    private int radius;
+
+    public Circle(int x, int y, int radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public Circle(Circle source) { // Copy Constructor
+        this.x = source.x;
+        this.y = source.y;
+        this.radius = source.radius;
+    }
+
+    @Override
+    public Shape clone() {
+        return new Circle(this); // Using copy constructor
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing circle at (" + x + ", " + y + ") with radius " + radius);
+    }
+}
+
+// Client Code
+public class PrototypeDemo {
+    public static void main(String[] args) {
+        Circle originalCircle = new Circle(10, 10, 5);
+        originalCircle.draw();
+
+        Shape clonedCircle = originalCircle.clone();
+        clonedCircle.draw();
+
+        // Modify the cloned circle
+        Circle circle = (Circle) clonedCircle; // cast to access Circle fields
+        circle.x = 20;
+        circle.y = 20;
+        circle.radius = 8;
+
+        clonedCircle.draw(); // cloned circle has the new values
+
+        originalCircle.draw(); // original circle is unchanged.
+    }
+}
