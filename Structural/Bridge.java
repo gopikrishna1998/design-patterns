9846bea87c8835b24ed4662859a050093c506150
gopikrishna1998 <32165792+gopikrishna1998@users.noreply.github.com>
2025-05-17 00:07:49 +0530

    Making AdvancedRemote extend BasicRemote

--- Structural/Bridge.java
+++ Structural/Bridge.java
@@ -75,24 +75,9 @@ class BasicRemote extends Remote {
     }
 }
 
-class AdvancedRemote extends Remote {
+class AdvancedRemote extends BasicRemote {
     public AdvancedRemote(Device device) {
         super(device);
-    }
-
-    @Override
-    public void turnOn() {
-        device.turnOn();
-    }
-
-    @Override
-    public void turnOff() {
-        device.turnOff();
-    }
-
-    @Override
-    public void setChannel(int channel) {
-        device.setChannel(channel);
     }
 
     public void mute() {
