2b1cea7a2b7d1752e131f2570820012fb51adc31
gopikrishna1998 <gopikrishna1998@gmail.com>
2025-05-17 22:21:41 +0530

    Adding comments for Singleton

--- Creational/Singleton.java
+++ Creational/Singleton.java
@@ -1,6 +1,8 @@
 public final class Singleton {
-    // The field must be declared volatile so that double check lock would work
-    // correctly.
+    
+    // volatile is used so that reordering of the instructions is not done and
+    // variables are read and written directly to memory.
+
     private static volatile Singleton instance;
 
     public String value;
@@ -10,23 +12,22 @@ public final class Singleton {
     }
 
     public static Singleton getInstance(String value) {
-        // The approach taken here is called double-checked locking (DCL). It
-        // exists to prevent race condition between multiple threads that may
-        // attempt to get singleton instance at the same time, creating separate
-        // instances as a result.
-        //
-        // It may seem that having the `result` variable here is completely
-        // pointless. There is, however, a very important caveat when
-        // implementing double-checked locking in Java, which is solved by
-        // introducing this local variable.
-        //
-        // You can read more info DCL issues in Java here:
-        // https://refactoring.guru/java-dcl-issue
+
+        // local variable is used because reading a volatile variable
+        // is more expensive than reading a local variable.
+
         Singleton result = instance;
         if (result != null) {
             return result;
         }
+
+        // synchronized used to ensure only one thread creates the Singleton instance.
+
         synchronized(Singleton.class) {
+            
+            // Multiple threads could pass the first if (result != null) check.
+            // When they reach the synchronized block, they must check again to see if the instance is still null.
+            
             if (instance == null) {
                 instance = new Singleton(value);
             }
