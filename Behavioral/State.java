0a61c26ea5a22fb6de5937943968bcae2446a9c0
gopikrishna1998 <32165792+gopikrishna1998@users.noreply.github.com>
2025-08-22 09:10:49 +0530

    Renaming variables

--- Behavioral/State.java
+++ Behavioral/State.java
@@ -1,57 +1,57 @@
 // State Interface
 interface TrafficLightState {
-    void handle(TrafficLight context);
+    void handle(TrafficLight trafficLight);
 }
 
 // Concrete States
 class RedState implements TrafficLightState {
     @Override
-    public void handle(TrafficLight context) {
+    public void handle(TrafficLight trafficLight) {
         System.out.println("Traffic Light: Red");
         try {
             Thread.sleep(2000); // Simulate red light duration
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        context.setState(new GreenState());
+        trafficLight.setState(new GreenState());
     }
 }
 
 class YellowState implements TrafficLightState {
     @Override
-    public void handle(TrafficLight context) {
+    public void handle(TrafficLight trafficLight) {
         System.out.println("Traffic Light: Yellow");
         try {
             Thread.sleep(1000); // Simulate yellow light duration
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        context.setState(new RedState());
+        trafficLight.setState(new RedState());
     }
 }
 
 class GreenState implements TrafficLightState {
     @Override
-    public void handle(TrafficLight context) {
+    public void handle(TrafficLight trafficLight) {
         System.out.println("Traffic Light: Green");
         try {
             Thread.sleep(3000); // Simulate green light duration
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        context.setState(new YellowState());
+        trafficLight.setState(new YellowState());
     }
 }
 
 // Context
 class TrafficLight {
-    private TrafficLightState state;
+    private TrafficLightState trafficLightState;
 
     public TrafficLight() {
         state = new RedState(); // Initial state
     }
 
-    public void setState(TrafficLightState state) {
+    public void setState(TrafficLightState trafficLightState) {
         this.state = state;
     }
 
